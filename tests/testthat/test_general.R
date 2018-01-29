ef89d7d6b1455145b53b5622e5148e376e6e984a
OganM <ogan.mancarcii@gmail.com>
2018-01-29 14:38:01 -0800

    test for long input

--- tests/testthat/test_general.R
+++ tests/testthat/test_general.R
@@ -2,9 +2,16 @@ context('basic functionality check')
 
 
 test_that('synonymCheck',{
-    expect_equal(humanSyno('TMSB10') %>% class,'list')
+    expect_is(humanSyno('TMSB10'),'list')
+    
     expect_warning(humanSyno('TADSADSDA'),"Returning own name")
     expect_silent(humanSyno('TMSB10'))
     
     expect_equal(mouseSyno("Eno2") %>% class, 'list')
+    
+    # long inputs
+    mouseSymbols = syno10090 %>% strsplit('[|]') %>% sapply(function(x){x[[1]]})
+    
+    expect_silent(mouseSyno(mouseSymbols[1:50]))
+    expect_is(mouseSyno(mouseSymbols[1:50]),'list')
 })
