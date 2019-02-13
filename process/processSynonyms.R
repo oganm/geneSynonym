475d64c38a51510f1e8e123b8f7a409768dae31f
Ogan Mancarci <ogan.mancarci@gmail.com>
2019-02-12 19:53:26 -0800

    less memory use when updating

--- process/processSynonyms.R
+++ process/processSynonyms.R
@@ -6,28 +6,17 @@ library(ogbox)
 library(glue)
 library(R.utils)
 
+autogit = TRUE
+
 devtools::use_data_raw()
+
+# get tax data -----
 
 if(!file.exists('data-raw/taxdump/names.dmp')){
     download.file(url ='ftp://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz', destfile = "data-raw/taxdump.tar.gz")
     dir.create('data-raw/taxdump', showWarnings = FALSE)
     untar('data-raw/taxdump.tar.gz',exdir = 'data-raw/taxdump/')
 }
-
-
-download.file('ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/gene_info.gz','data-raw/gene_info.gz')
-R.utils::gunzip('data-raw/gene_info.gz', overwrite = TRUE)
-geneInfo = fread('data-raw/gene_info',sep='\t',skip=1, header = F,data.table = FALSE)
-setnames(geneInfo,old = names(geneInfo),new=
-             c('tax_id', 'GeneID', 'Symbol', 'LocusTag', 'Synonyms',
-               'dbXrefs', 'chromosome', 'map_location', 'description',
-               'type_of_gene', 'Symbol_from_nomenclature_authority',
-               'Full_name_from_nomenclature_authority', 'Nomenclature_status',
-               'Other_designations', 'Modification_date','Feature_type'))
-
-
-
-geneInfo = geneInfo[,c('Symbol','GeneID','Synonyms','Symbol_from_nomenclature_authority','tax_id')]
 
 taxData = fread('data-raw/taxdump/names.dmp',data.table=FALSE, sep = '\t',quote = "")
 taxData = taxData[c(1,3,5,7)]
@@ -45,7 +34,50 @@ taxData %>% apply(1,function(x){
     dataDocs = glue('#\' Synonym information for {x["name_txt"]}\n"syno{x["tax_id"] %>% trimws}"')
 }) %>% paste(collapse='\n\n') %>% writeLines('R/dataDocumentation.R')
 
-devtools::document()
+
+if(autogit){
+    git2r::add(repo,path = 'R/dataDocumentation.R')    
+}
+
+
+# get gene_info --------
+
+download.file('ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/gene_info.gz','data-raw/gene_info.gz')
+R.utils::gunzip('data-raw/gene_info.gz', overwrite = TRUE)
+
+if(Sys.info()['sysname'] == 'Windows'){
+    if(!pkgbuild::has_rtools()){
+        stop('install rtools')
+    }
+    rtoolsPath = pkgbuild::rtools_path()
+    
+    grep = file.path(rtoolsPath,'grep.exe')
+    
+} else{
+    grep = 'grep'
+}
+
+regexTaxID = paste0('\t',ogbox::regexMerge(taxData$tax_id),'\t')
+
+system2(grep,
+        c('-E',regexTaxID,'data-raw/gene_info'),
+        stderr = 'data-raw/gene_info_filtered',
+        stdout = 'data-raw/gene_info_filtered')
+
+geneInfo = fread('data-raw/gene_info_filtered',sep='\t',skip=1, header = F,data.table = FALSE)
+setnames(geneInfo,old = names(geneInfo),new=
+             c('tax_id', 'GeneID', 'Symbol', 'LocusTag', 'Synonyms',
+               'dbXrefs', 'chromosome', 'map_location', 'description',
+               'type_of_gene', 'Symbol_from_nomenclature_authority',
+               'Full_name_from_nomenclature_authority', 'Nomenclature_status',
+               'Other_designations', 'Modification_date','Feature_type'))
+
+
+
+geneInfo = geneInfo[,c('Symbol','GeneID','Synonyms','Symbol_from_nomenclature_authority','tax_id')]
+
+
+
 
 tax = taxData$tax_id
 
@@ -67,7 +99,6 @@ synos = sapply(1:nrow(geneInfo),function(i){
 names(synos) = geneInfo$GeneID
 repo = repository('.')
 
-git2r::add(repo,path = 'R/dataDocumentation.R')
 
 # file generation
 for (i in tax){
@@ -79,13 +110,18 @@ for (i in tax){
     # teval(paste0('cat(syno',i,', file="data-raw/syno',i,'",sep="\n")'))
     
     teval(paste0('rm(syno',i,',envir = .GlobalEnv)'))
-    git2r::add(repo,path =paste0('data-raw/syno',i))
-    git2r::add(repo,path =paste0('data/syno',i,'.rda'))
+    if(autogit){
+        git2r::add(repo,path =paste0('data-raw/syno',i))
+        git2r::add(repo,path =paste0('data/syno',i,'.rda'))
+    }
     Sys.sleep(2)
 }
 
 devtools::document()
-git2r::add(repo,path = 'man/')
+if(autogit){
+    git2r::add(repo,path = 'man/')    
+}
+
 
 # version update add date to the version as a revision
 version = ogbox::getVersion()
@@ -95,17 +131,17 @@ ogbox::setVersion(paste(version[1],version[2],
                             gsub(pattern = '\\.0','.',x=.),sep='.'))
 ogbox::setDate(format(Sys.Date(),'%Y-%m-%d'))
 
-git2r::add(repo,path ='DESCRIPTION')
-
-tryCatch({
-    git2r::commit(repo,message = paste('Weekly auto update'))
-    token = readLines('data-raw/auth')
-    Sys.setenv(GITHUB_PAT = token)
-    cred = git2r::cred_token()
-    git2r::push(repo,credentials = cred)
+if(autogit){
+    git2r::add(repo,path ='DESCRIPTION')
+    
+    tryCatch({
+        git2r::commit(repo,message = paste('Weekly auto update'))
+        token = readLines('data-raw/auth')
+        Sys.setenv(GITHUB_PAT = token)
+        cred = git2r::cred_token()
+        git2r::push(repo,credentials = cred)
     },
     error = function(e){
         print('nothing to update')
     })
-
-
+}
