7c9734eb1a33f2d5e0b83f42172c1d57eec664e0
OganM <ogan.mancarcii@gmail.com>
2021-11-12 14:53:20 -0800

    error logging

--- process/autoUpdate.sh
+++ process/autoUpdate.sh
@@ -3,4 +3,4 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 cd "$parent_path"
 cd ..
-Rscript 'process/processSynonyms.R'
+Rscript 'process/processSynonyms.R'> process/prepLog 2>process/prepLogErr
